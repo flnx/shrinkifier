4d8c40c1c0c5dbe598eaf577841db4891e0e5096
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2024-06-15 23:01:58 +0300

    filter out files bigger than 4mb

--- src/components/drag-and-drop/DragAndDrop.tsx
+++ src/components/drag-and-drop/DragAndDrop.tsx
@@ -7,9 +7,11 @@ type DragAndDropProps = {
 };
 
 export function DragAndDrop({ handleFiles }: DragAndDropProps) {
+  const MAX_FILE_SIZE = 4 * 1024 * 1024; // 4 MB in bytes
+
   const handleOnSelectChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = Array.from(e.target.files || []).filter((file) =>
-      file.type.startsWith('image/')
+    const files = Array.from(e.target.files || []).filter(
+      (file) => file.type.startsWith('image/') && file.size < MAX_FILE_SIZE
     );
 
     handleFiles(files);
@@ -19,8 +21,8 @@ export function DragAndDrop({ handleFiles }: DragAndDropProps) {
   const handleDrop = (e: React.DragEvent) => {
     e?.preventDefault();
 
-    const files = Array.from(e.dataTransfer.files).filter((file) =>
-      file.type.startsWith('image/')
+    const files = Array.from(e.dataTransfer.files).filter(
+      (file) => file.type.startsWith('image/') && file.size < MAX_FILE_SIZE
     );
 
     handleFiles(files);
